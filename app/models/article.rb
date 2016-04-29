d2cc840bd1cbd4b1a3867cae1ee7957739e7f219
Brian <bbensch@gmail.com>
2016-04-29 16:47:15 -0700

    clean up index page, set devise timeout to 30days, add title to params

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,11 @@ class Article < ActiveRecord::Base
   require 'aws-sdk-v1'
   require 'aws-sdk'
 
-    def time_since_written
+  def to_param
+    [id, title.parameterize].join("-")
+  end
+
+  def time_since_written
     time_elapsed = ((Date.today - published) / 1).round
       if time_elapsed < 1
         return "today."
@@ -10,4 +14,5 @@ class Article < ActiveRecord::Base
         return "#{time_elapsed} days ago."
       end
   end
+
 end
