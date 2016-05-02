9753855a1c195e9bcd6c27e63f9da3bfd3d81ffb
Brian <bbensch@gmail.com>
2016-05-02 15:14:21 -0700

    update seed file, TSing CKeditor bugs...?

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,29 @@ User.delete_all
 Profile.delete_all
 Snippet.delete_all
 
-#CREATE LIST OF OPTIONS TO RANDOMLY SELECT FROM
-age_array = (18...65).to_a
-political_affiliation_array = ['Democrat','Democrat','Republican','Republican','Independent']
+User.create!({
+  email:"bbensch@gmail.com",
+  password:"bb4ste109"
+  })
 
-20.times do
-  User.create!({
-      email: Faker::Internet.safe_email,
-      password: 'password'
-    })
-  Profile.create!({
-      user_id: User.last.id,
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      city: Faker::Address.city,
-      state: Faker::Address.state_abbr,
-      age: age_array.sample,
-      about_me: Faker::Hacker.say_something_smart,
-      display_name: Faker::Internet.user_name,
-      political_affiliation: political_affiliation_array.sample
-    })
-end
+#CREATE LIST OF OPTIONS TO RANDOMLY SELECT FROM
+# age_array = (18...65).to_a
+# political_affiliation_array = ['Democrat','Democrat','Republican','Republican','Independent']
+
+# 20.times do
+#   User.create!({
+#       email: Faker::Internet.safe_email,
+#       password: 'password'
+#     })
+#   Profile.create!({
+#       user_id: User.last.id,
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       city: Faker::Address.city,
+#       state: Faker::Address.state_abbr,
+#       age: age_array.sample,
+#       about_me: Faker::Hacker.say_something_smart,
+#       display_name: Faker::Internet.user_name,
+#       political_affiliation: political_affiliation_array.sample
+#     })
+# end
